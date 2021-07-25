7d2f96f0f73cbce5d079c3d8ba55351e7159a814
Maksym Zenzeliuk <zenzelyuk.m@gmail.com>
2021-07-26 00:07:43 +0300

    Continue

---
+++ src/com/examples/synchronized_examples/Example_1.java
@@ -0,0 +1,26 @@
+package com.examples.synchronized_examples;
+
+public class Example_1 {
+
+    public static void main(String[] args) {
+        Thread t = new MyThread();
+        t.start();
+        System.out.println("OK");
+    }
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println(getName());
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
