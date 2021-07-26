01547bcaf2f2dd046ea90b022d3b478c8ade103c
Maksym Zenzeliuk <zenzelyuk.m@gmail.com>
2021-07-26 23:33:14 +0300

    Continue

--- src/com/examples/synchronized_examples/Example_1.java
+++ src/com/examples/synchronized_examples/Example_1.java
@@ -2,25 +2,34 @@ package com.examples.synchronized_examples;
 
 public class Example_1 {
 
+    boolean flag;
+
+    void m(boolean f) {
+        this.flag = f;
+        try {
+            Thread.sleep(333);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(flag + " == " + f);
+    }
+
+
     public static void main(String[] args) {
-        Thread t = new MyThread();
-        t.start();
-        System.out.println("OK");
+        Example_1 e = new Example_1();
+        new Thread(() -> {
+            while (true){
+                e.m(false);
+            }
+        }).start();
+
+        new Thread(() -> {
+            while (true){
+                e.m(true);
+            }
+        }).start();
     }
 }
 
-class MyThread extends Thread {
-    @Override
-    public void run() {
-        while (true) {
-            System.out.println(getName());
-            try {
-                sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-}
 
 
